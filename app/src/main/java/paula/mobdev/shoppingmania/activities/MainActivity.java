b502688339b5414fc0201e87e68f11e9e4511b16
PBBassily <pbbassily@gmail.com>
2018-08-27 11:27:18 +0200

    creating the item model

--- app/src/main/java/paula/mobdev/shoppingmania/activities/MainActivity.java
+++ app/src/main/java/paula/mobdev/shoppingmania/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package paula.mobdev.shoppingmania;
+package paula.mobdev.shoppingmania.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import paula.mobdev.shoppingmania.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/paula/mobdev/shoppingmania/model/Item.java
@@ -0,0 +1,69 @@
+package paula.mobdev.shoppingmania.model;
+
+/**
+ * POJO class for representing the building
+ * unit of this application in JAVA, which
+ * is the item object
+ */
+public class Item {
+    // the id used by the DB
+    private Integer id;
+
+    private String name;
+
+    // used to sort item by category
+    private String category;
+
+    private double price;
+
+    // whether done or not
+    private boolean checked = false;
+
+    /**
+     *constructor for creating an item
+     */
+
+    public Item(Integer id,String name, String category, double price, boolean checked) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.checked = checked;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public boolean getStatus(){
+        return checked ;
+    }
+    public void setStatus(boolean checked){
+        this.checked=checked;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+}
